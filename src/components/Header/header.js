defaf96831563fc5db514f949e5234f350af906d
Rao Daniyal <raodaniyal1995@yahoo.com>
2020-01-27 14:28:09 +0500

    home page header work

---
+++ src/components/Header/header.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './header.scss'
+import image from '../../assets/image.jpg'
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="left-side">
+        <div className="content">
+          <h1>Bring your brand to Life with our <br /> help</h1>
+          <p>A guy who loves Coding, Travelling, Sunsets, Beaches and waking up early in the morning. <br />
+            ~ Software Engineer</p>
+          <button className="contact-btn">Contact Me /&gt;</button>
+        </div>
+        <div className="social-links">
+          <a className=""><i className="fab fa-facebook-f"></i>Facebook</a>
+          <a className=""><i className="fab fa-twitter"></i>Twitter</a>
+          <a className=""><i className="fab fa-linkedin-in"></i>LinkedIn</a>
+          <a className=""><i className="fab fa-github"></i>Github</a>
+          {/* <a>Upwork</a>
+        <a>Fiverr</a> */}
+        </div>
+      </div>
+      <div className="right-side">
+        <div className="image-container">
+          <img src={image} />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
--- src/container/Home/index.js
+++ src/container/Home/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './home.scss'
+import Header from '../../components/Header/header'
 
 function Home() {
   return (
     <div className="container">
-        <p>home</p>
+        <Header/>
     </div>
   );
 }
