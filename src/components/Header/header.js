6b996f4a9d478856d21875e2b932be6df0de63ab
Rao Daniyal <raodaniyal1995@yahoo.com>
2020-11-06 16:30:01 +0500

    gsap integration

--- src/components/Header/header.js
+++ src/components/Header/header.js
@@ -1,22 +1,45 @@
-import React from 'react';
+import React, { useRef, useEffect } from "react";
 import './header.scss'
 import image from '../../assets/image.jpg'
+import { TweenMax } from "gsap";
 
-function Header() {
+const Header = () => {
+  const blue = useRef(null);
+  const red = useRef(null);
+  const yellow = useRef(null);
+  const green = useRef(null);
+
+  useEffect(() => {
+    TweenMax.fromTo(
+      [blue.current, yellow.current],
+      0.5,
+      { y: 18 },
+      { y: -18, yoyo: true, repeat: -1 }
+    );
+    TweenMax.fromTo(
+      [red.current, green.current],
+      0.5,
+      { y: -18 },
+      { y: 18, repeat: -1, yoyo: true }
+    );
+  }, []);
+
+
+
   return (
     <div className="header">
       <div className="left-side">
         <div className="content">
-          <h1>Frontend UI Developer</h1>
+          <h1 >Frontend UI Developer</h1>
           <p>Bring your brand to Life with our help</p>
           <p>A guy who loves Coding, Travelling, Sunsets, Beaches and waking up early in the morning. <br />
             ~ Software Engineer</p>
           <button className="contact-btn">Contact Me /&gt;</button>
           <div className="social-links">
-            <a className=""><i className="fab fa-facebook-f"></i>Facebook</a>
-            <a className=""><i className="fab fa-twitter"></i>Twitter</a>
-            <a className=""><i className="fab fa-linkedin-in"></i>LinkedIn</a>
-            <a className=""><i className="fab fa-github"></i>Github</a>
+            <a ref={blue} className=""><i className="fab fa-facebook-f"></i>Facebook</a>
+            <a ref={red} className=""><i className="fab fa-twitter"></i>Twitter</a>
+            <a ref={yellow} className=""><i className="fab fa-linkedin-in"></i>LinkedIn</a>
+            <a ref={green} className=""><i className="fab fa-github"></i>Github</a>
           </div>
         </div>
       </div>
